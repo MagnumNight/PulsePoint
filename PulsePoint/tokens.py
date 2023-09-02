4241976d9c3e787a6c2e5961135c68ccf75df1ce
Kevin Ifill <kevinifill1998@gmail.com>
2023-09-02 15:26:38 -0400

    Updating Email Confirmation

--- PulsePoint/tokens.py
+++ PulsePoint/tokens.py
@@ -7,8 +7,4 @@ class TokenGenerator(PasswordResetTokenGenerator):
             six.text_type(user.pk) + six.text_type(timestamp) +
             six.text_type(user.is_active)
         )
-<<<<<<< HEAD
 account_activation_token = TokenGenerator()
-=======
-account_activation_token = TokenGenerator()
->>>>>>> fa42bb77b86d5fd77c2128ec4cbee5e48319839d
