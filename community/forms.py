daa17bce6120b7532a3e9db3578a21509b2a01ee
alroney <andrewroney@rocketmail.com>
2023-09-05 12:57:08 -0400

    Changed naming

--- community/forms.py
+++ community/forms.py
@@ -4,4 +4,4 @@ from .models import Forum
 class ForumForm(forms.ModelForm):
     class Meta:
         model = Forum
-        fields = ['forum_title', 'forum_description', 'forum_date_created']
+        fields = ['title', 'description', 'date_created']
--- community/models.py
+++ community/models.py
@@ -6,9 +6,9 @@ from django.db import models
 
 # Create your models here.
 class Forum(models.Model):
-    forum_title: models.CharField(max_length=100)
-    forum_description: models.TextField()
-    forum_date_created: models.DateTimeField(auto_now_add=True)
+    title: models.CharField(max_length=100)
+    description: models.TextField()
+    date_created: models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
