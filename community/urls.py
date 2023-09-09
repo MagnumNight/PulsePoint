06a42ef6da40b0f406495d94a8e486ae9457ceac
alroney <andrewroney@rocketmail.com>
2023-09-09 08:37:55 -0400

    Pulled from Main on 9/9/2023

--- community/urls.py
+++ community/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = "community"
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path("", views.home, name="home"),
+    path("create_forum/", views.create_forum, name="create_forum"),
+    path(
+        "forum/<int:forum_id>/", views.forum_detail, name="forum_detail"
+    ),
 ]
