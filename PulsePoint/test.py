888b030e8abf03043cee9b5914d615650216fab3
kifill1998 <97985598+kifill1998@users.noreply.github.com>
2023-09-16 13:01:09 -0400

    Additional Login Cases / Added Registration Test Cases

--- PulsePoint/test.py
+++ PulsePoint/test.py
@@ -18,3 +18,27 @@ class LoginTestCase(TestCase):
         user = User.objects.create_user(username='usertesting', password='password')
         response = self.client.post(reverse('login'),{'username': 'user', 'password': 'password'})
         self.assertEqual(response.status_code, 200)
+
+    def test_empty_username(self):
+        user = User.objects.create_user(username='usertesting', password='password')
+        response = self.client.post(reverse('login'),{'username': '', 'password': 'password'})
+        self.assertEqual(response.status_code, 200)
+
+    def test_empty_password(self):
+        user = User.objects.create_user(username='usertesting', password='password')
+        response = self.client.post(reverse('login'),{'username': 'usertesting', 'password': ''})
+        self.assertEqual(response.status_code, 200)
+
+class RegisterTestCase(TestCase):
+    def test_valid_signup(self):
+        response = self.client.post(reverse('signup'), {'first_name': 'first', 'last_name': 'last', 'username': 'testuser', 'email':'testing@gmail.com', 'password1': 'testing1234!', 'password2': 'testing1234!'})
+        self.assertEqual(response.status_code, 200)
+
+    def test_weak_password(self):
+        response = self.client.post(reverse('signup'), {'first_name': 'first', 'last_name': 'last', 'username': 'testuser', 'email':'testing@gmail.com', 'password1': '1234', 'password2': '1234'})
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_nonunique_user(self):
+        user = User.objects.create_user(username='usertesting', password='password')
+        response = self.client.post(reverse('signup'), {'first_name': 'first', 'last_name': 'last', 'username': 'usertesting', 'email':'testing@gmail.com', 'password1': '1234', 'password2': '1234'})
+        self.assertEqual(response.status_code, 200)
