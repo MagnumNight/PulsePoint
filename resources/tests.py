e7df25731bdb6e1c8d5c377f50c68b2745a79daf
kifill1998 <97985598+kifill1998@users.noreply.github.com>
2023-09-16 14:58:59 -0400

    Adding single test for resources - extension for Circle CI

--- resources/tests.py
+++ resources/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your tests here.
+class ResourceTestCase(TestCase):
+
+    def test_resource_access(self):
+         response = self.client.post(reverse('resources:home'))
+         self.assertEqual(response.status_code, 200)
