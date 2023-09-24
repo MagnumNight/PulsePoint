cc58f7d35c924e57cf966fe4bdaf47666b1a3e8e
kifill1998 <97985598+kifill1998@users.noreply.github.com>
2023-09-24 17:11:57 -0400

    Additional cases for resources tab.

--- resources/tests.py
+++ resources/tests.py
@@ -8,3 +8,28 @@ class ResourceTestCase(TestCase):
     def test_resource_access(self):
          response = self.client.post(reverse('resources:home'))
          self.assertEqual(response.status_code, 200)
+
+    def test_link_resources(self):
+        response = self.client.post(reverse('resources:home'))
+
+        self.assertContains(response, "General Anxiety Disorder")
+        self.assertContains(response, "https://adaa.org/understanding-anxiety/generalized-anxiety-disorder-gad")
+
+        self.assertContains(response, "Post Traumatic Stress Disorder")
+        self.assertContains(response, "https://www.maketheconnection.net/conditions/ptsd/")
+
+        self.assertContains(response, "Bipolar Disorder Type I and II")
+        self.assertContains(response, "https://www.nimh.nih.gov/health/topics/bipolar-disorder")
+
+        self.assertContains(response, "Obsessive Compulsive Disorder")
+        self.assertContains(response, "https://iocdf.org/about-ocd/")
+
+        self.assertContains(response, "Major Depressive Disorder")
+        self.assertContains(response, "https://www.psychiatry.org/patients-families/depression/what-is-depression")
+
+        self.assertContains(response, "Attention-Deficit Hyperactivity Disorder")
+        self.assertContains(response, "https://www.nimh.nih.gov/health/topics/attention-deficit-hyperactivity-disorder-adhd")
+
+
+        
+
