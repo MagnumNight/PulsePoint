d916712e0e705f2ab4609780c34aac75aecb0866
kifill1998 <97985598+kifill1998@users.noreply.github.com>
2023-09-24 19:00:35 -0400

    Additional tests for Delete Account and Email Verification.

--- PulsePoint/test.py
+++ PulsePoint/test.py
@@ -42,3 +42,31 @@ class RegisterTestCase(TestCase):
         user = User.objects.create_user(username='usertesting', password='password')
         response = self.client.post(reverse('signup'), {'first_name': 'first', 'last_name': 'last', 'username': 'usertesting', 'email':'testing@gmail.com', 'password1': '1234', 'password2': '1234'})
         self.assertEqual(response.status_code, 200)
+
+
+    def test_email_verification(self):
+        response = self.client.post(reverse('signup'), {'first_name': 'first', 'last_name': 'last', 'username': 'testuser', 'email':'testing@gmail.com', 'password1': 'testing1234!', 'password2': 'testing1234!'})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/email_confirmation.html')
+
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].subject, "Activate your PulsePoint account.")
+
+        user = User.objects.get(username = 'testuser')
+        uidb64 = urlsafe_base64_encode(force_bytes(user.pk))
+        token = account_activation_token.make_token(user)
+        response = self.client.get(reverse('activate', arg= [uidb64, token]))
+        self.assertEqual(response.status_code, 302)
+
+    def test_account_delete(self):
+        user = User.objects.create_user(username='usertesting', password='testing1234!')
+        response = self.client.post(reverse('login'),{'username': 'usertesting', 'password': 'testing1234!'})
+
+        response = self.client.get(reverse('delete_account'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/delete_account.html')
+
+        response = self.client.post(reverse('delete_account'))
+        self.assertEqual(response.status_code, 302)
+
+        self.assertFalse(User.objects.filter(username = 'usertesting').exists())
